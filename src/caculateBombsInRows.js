63df3c917c19c2da5a35eb4c30c0686945218e67
Neal Lubin <naftalilubin@gmail.com>
2017-07-28 12:50:19 -0400

    Start bomb seed at a random block on the board

--- src/caculateBombsInRows.js
+++ src/caculateBombsInRows.js
@@ -4,6 +4,7 @@ import {expandOutFromBlock} from "./expandOutFromBlock";
 const MAX_BOMBS_NEXT_TO_EACH_OTHER_IN_ROW = 2;
 // not the best randomizer, but can be optimized later
 const bombSeed = () => Math.random() > 0.8;
+const generateRandomFromZero = (max) => Math.floor(Math.random() * (max - 1));
 
 /**
  * Currently there is a bug in this code in that it will create at MAX the
@@ -31,41 +32,41 @@ export const calculateBombsInRows = ({ rows, columns, bombs, noBomb = {} }) => {
             emptyCells[`${row}|${column}`] = true;
         }
     };
-    
+
     const addBomb = ({ rowIndex, boxIndex, box, board }) => {
         if (box.containsBomb) {
             // remove from no-bombs list
             delete emptyCells[`${rowIndex}|${boxIndex}`];
-            
+
             if (boxIndex > 0) {
                 increaseCellValue({ row: rowIndex, column: boxIndex - 1 }, board);
-                
+
                 // check corner diagonals
                 if (rowIndex > 0) {
                     increaseCellValue({ row: rowIndex - 1, column: boxIndex - 1 }, board);
                 }
-                
+
                 if (rowIndex < (rows - 1)) {
                     increaseCellValue({ row: rowIndex + 1, column: boxIndex - 1 }, board);
                 }
             }
             if (boxIndex < (columns - 1)) {
                 increaseCellValue({ row: rowIndex, column: boxIndex + 1 }, board);
-                
+
                 // check corner diagonals
                 if (rowIndex > 0) {
                     increaseCellValue({ row: rowIndex - 1, column: boxIndex + 1 }, board);
                 }
-                
+
                 if (rowIndex < (rows - 1)) {
                     increaseCellValue({ row: rowIndex + 1, column: boxIndex + 1 }, board);
                 }
             }
-            
+
             if (rowIndex > 0) {
                 increaseCellValue({ row: rowIndex - 1, column: boxIndex }, board);
             }
-            
+
             if (rowIndex < (rows - 1)) {
                 increaseCellValue({ row: rowIndex + 1, column: boxIndex }, board);
             }
@@ -88,8 +89,8 @@ export const calculateBombsInRows = ({ rows, columns, bombs, noBomb = {} }) => {
         board.push(rowArray);
     }
 
-    const noBombRow = noBomb.row || 0;
-    const noBombColumn = noBomb.column || 0;
+    const noBombRow = noBomb.row || generateRandomFromZero(rows);
+    const noBombColumn = noBomb.column || generateRandomFromZero(columns);
     let hasNotPassedThroughYet = true;
 
     // seed the board starting either from the noBomb cell or from the top corner
@@ -130,7 +131,7 @@ export const calculateBombsInRows = ({ rows, columns, bombs, noBomb = {} }) => {
     if (bombCount !== bombs) {
         const emptyCellMap = Object.keys(emptyCells).map((cell) => {
             const [ r, c ] = cell.split('|');
-            
+
             return {
                 rowIndex: Number(r),
                 boxIndex: Number(c),
@@ -140,9 +141,9 @@ export const calculateBombsInRows = ({ rows, columns, bombs, noBomb = {} }) => {
 
         for (let i = 0; i < emptyCellMap.length && bombCount <= bombs; ++i) {
             const { rowIndex, boxIndex, box } = emptyCellMap[i];
-    
+
             const cannotHaveBomb = noBomb.row === rowIndex && noBomb.column === boxIndex;
-            
+
             if (!cannotHaveBomb) {
                 box.containsBomb = true;
                 bombCount += 1;
